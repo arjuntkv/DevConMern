ced722cac62937e1e40ac8b81b11f57f86ab5c38
Arjun TKV <arjun.tkv7@gmail.com>
2020-05-11 12:53:15 +0530

    Completed User Profile

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import EditProfile from "./components/profile-forms/EditProfile";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 //Redux
 import { Provider } from "react-redux";
@@ -42,6 +44,9 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
+
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
